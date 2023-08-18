62dfb4c99ef18990f41625283952e0bf4fa8aab9
Sushij <sushant.wurk@gmail.com>
2023-08-18 20:00:19 +0530

    web: RTK-query

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -1,12 +1,18 @@
-import { Box, useMediaQuery } from "@mui/material";
-import { Outlet } from "react-router-dom";
-import Navbar from "./Navbar";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import { Box, useMediaQuery } from "@mui/material";
+
+import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
+import { useGetUserQuery } from "../features/api";
+
+const USER_ID = "64df0c633a0f634577f9ac1c";
 
 export default function Layout() {
   const isMobile = useMediaQuery("(max-width: 600px)");
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(true);
+
+  const { data } = useGetUserQuery(USER_ID);
   return (
     <Box display={!isMobile ? "flex" : "block"} width={"100%"} height={"100%"}>
       <Sidebar
@@ -18,6 +24,7 @@ export default function Layout() {
       <Box flexGrow={1}>
         <Navbar sidebarOpen setSidebarOpen={setSidebarOpen} />
         <Outlet />
+        <pre>{JSON.stringify(data, null, 2)}</pre>
       </Box>
     </Box>
   );
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+
 import App from "./App.tsx";
 import "./index.css";
+import { api } from "./features/api.ts";
 import { ModeProvider } from "./context/Mode.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ModeProvider>
-      <App />
-    </ModeProvider>
+    <ApiProvider api={api}>
+      <ModeProvider>
+        <App />
+      </ModeProvider>
+    </ApiProvider>
   </React.StrictMode>
 );
